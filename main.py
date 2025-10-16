20f33b32a196b9cfb1c0f779efe1c4a19433ab8d
memometo35 <t.a.metwally35@gmail.com>
2025-10-17 01:43:12 +0300

    finally found the bug

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from src.textSummarizer.pipleline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-from src.textSummarizer.logging import logger
+from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from textSummarizer.logging import logger
 
 STAGE_NAME = "Data Ingestion Stage"
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
-    logger.info(f">>>>>> stage {STAGE_NAME} complated <<<<<<\n\nx==========x")
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
     raise e
